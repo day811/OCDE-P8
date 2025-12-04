d6e0b3eb29f9937942cf4fb7e81498e5ca7bfabd
day811 <day811@laposte.net>
2025-12-04 13:40:32 +0100

    add --service arg

--- push-to-ecs.sh
+++ push-to-ecs.sh
@@ -4,16 +4,38 @@ source .env.ecs
 
 # Parse arguments
 BUILD=true
-if [[ "$1" == "--no-build" ]]; then
-    BUILD=false
-fi
+SERVICE_FILTER=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --no-build)
+            BUILD=false
+            shift
+            ;;
+        --service)
+            SERVICE_FILTER="$2"
+            shift 2
+            ;;
+        *)
+            echo "Usage: $0 [--no-build] [--service preprocessing|ingestion|quality-checker]"
+            exit 1
+            ;;
+    esac
+done
 
 # Login ECR
 aws ecr get-login-password --region $AWS_REGION | \
   docker login --username AWS --password-stdin \
   $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-for SERVICE in preprocessing ingestion quality-checker; do
+SERVICES=(preprocessing ingestion quality-checker)
+
+for SERVICE in "${SERVICES[@]}"; do
+    # Filtrer si --service spécifié
+    if [[ -n "$SERVICE_FILTER" && "$SERVICE" != "$SERVICE_FILTER" ]]; then
+        continue
+    fi
+    
     IMAGE_NAME="greencoop/$SERVICE"
     ECR_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest"
     DOCKERFILE="src/${SERVICE//-/_}/Dockerfile"
